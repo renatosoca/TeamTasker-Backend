927a6f87f8c05eb035eb2e8fec0fc6b6e82b3024
DevRenato <u18215194@gmail.com>
2023-03-30 01:16:01 -0500

    crud collaborators

--- src/controllers/projectController.ts
+++ src/controllers/projectController.ts
@@ -1,11 +1,11 @@
-import { Request, Response } from 'express'
+import { Response } from 'express'
 import { errorHttp } from '../helpers'
 import { UserResquestProvider } from '../interfaces'
 import { projectModel, userModel } from '../models'
 
 const getProjects = async ( { user }: UserResquestProvider, res: Response) => {
   try {
-    const projects = await projectModel.find().where('owner').equals(user?._id).populate('owner', 'name lastname email').populate('boards').sort({ createdAt: -1 });
+    const projects = await projectModel.find().where('owner').equals(user?._id).populate('owner', '_id name lastname username email').populate('boards').populate('collaborators', '_id name lastname username email').sort({ createdAt: -1 });
 
     return res.status(200).json({
       ok: true,
@@ -20,10 +20,10 @@ const getProject = async ( { params, user }: UserResquestProvider, res: Response
   const { id } = params;
   
   try {
-    const project = await projectModel.findById( id ).populate('boards').populate('collaborators');
+    const project = await projectModel.findById( id ).populate('boards').populate('collaborators', '_id name lastname username email').populate('owner', '_id name lastname username email');
     if ( !project ) return res.status(404).json({ ok: false, msg: 'No existe el proyecto' });
     
-    if ( project?.owner.toString() !== user?._id.toString() ) return res.status(403).json({ ok: false, msg: 'No autorizado para esta acción' });
+    if ( project?.owner._id.toString() !== user?._id.toString() ) return res.status(403).json({ ok: false, msg: 'No autorizado para esta acción' });
 
     return res.status(200).json({
       ok: true,
@@ -38,6 +38,7 @@ const createProject = async ( { body, user }: UserResquestProvider, res: Respons
   try {
     const project = new projectModel( body );
     project.owner = user?.id;
+    project.collaborators.push( user?.id );
 
     const newProject = await project.save();
 
@@ -96,7 +97,7 @@ const searchCollaborator = async ( { body }: UserResquestProvider, res: Response
   const { username } = body;
   
   try {
-    const users = await userModel.find({ $or: [{ username: { $regex: username, $options: "i" }}] });
+    const users = await userModel.find({ $or: [{ username: { $regex: username, $options: "i" }}] }).select('-password -token -confirmed -createdAt -updatedAt -__v');
     if ( !users ) return res.status(404).json({ ok: false, msg: 'No existen usuarios con ese nombre o email' });
 
     return res.status(200).json({
@@ -108,11 +109,58 @@ const searchCollaborator = async ( { body }: UserResquestProvider, res: Response
   }
 }
 
-const addCollaborator = (_req: Request ) => {
+const addCollaborator = async ( { params, body }: UserResquestProvider, res: Response ) => {
+  const { id } = params;
+  const { collaboratorId } = body;
+  
+  try {
+    const project = await projectModel.findById( id ).populate('boards').populate('collaborators', '_id name lastname username email').populate('owner', '_id name lastname username email');
+    if ( !project ) return res.status(404).json({ ok: false, msg: 'No existe el proyecto' });
 
+    const userExist = await userModel.findById( collaboratorId ).select('-password -token -confirmed -createdAt -updatedAt -__v');
+    if ( !userExist ) return res.status(404).json({ ok: false, msg: 'No existe el usuario' });
+    
+    if ( project.owner._id.toString() === userExist._id.toString() ) return res.status(403).json({ ok: false, msg: 'El administrador no puede ser colaborador' });
+
+    const isCollaborator = project.collaborators.some(collaborator => collaborator._id.toString() === userExist._id.toString());
+    if ( isCollaborator ) return res.status(403).json({ ok: false, msg: 'Este usuario ya es colaborador' });
+    
+    project.collaborators.push( userExist );
+    const savedProject = await project.save();
+    
+    return res.status(200).json({
+      ok: true,
+      project: savedProject,
+      msg: 'Colaborador agregado correctamente.'
+    })
+  } catch (error) {
+    return errorHttp( res, 'Error del sistema, comuniquese con el administrador');
+  }
 }
 
-const deleteCollaborator = (  ) => {
+const deleteCollaborator = async ( { params, body, user }: UserResquestProvider, res: Response ) => {
+  const { id } = params;
+  const { collaboratorId } = body;
+  
+  try {
+    const project = await projectModel.findById( id ).populate('collaborators', '_id name lastname username email');
+    if ( !project ) return res.status(404).json({ ok: false, msg: 'No existe el proyecto' });
+    
+    if ( project.owner._id.toString() !== user?._id.toString() ) return res.status(403).json({ ok: false, msg: 'No autorizado' });
+    if ( project.owner._id.toString() === collaboratorId.toString() ) return res.status(403).json({ ok: false, msg: 'El administrador no puede ser eliminado' });
+
+    project.collaborators = project.collaborators.filter(collaborator => collaborator._id.toString() !== collaboratorId.toString());
+
+    const savedProject = await project.save();
+    
+    return res.status(200).json({
+      ok: true,
+      project: savedProject,
+      msg: 'Colaborador eliminado correctamente.'
+    })
+  } catch (error) {
+    return errorHttp( res, 'Error del sistema, comuniquese con el administrador');
+  }
 }
 
 export {
--- src/interfaces/project.interface.ts
+++ src/interfaces/project.interface.ts
@@ -1,11 +1,12 @@
 import { Document, ObjectId } from 'mongoose';
+import { User } from './user.interface';
 
 export interface Project extends Document {
   _id: ObjectId | string;
   name: string;
   description: string,
   type: string;
-  owner: ObjectId | string;
+  owner: User;
   boards: ( ObjectId | string )[];
-  collaborators: ( ObjectId | string )[];
+  collaborators: ( User )[];
 }
